90d4a02d8749c15e2a3880fadb5ae596178d60d0
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-24 20:08:19 +0530

    Validating exactly one special character

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -10,7 +10,7 @@ namespace UserRegistrationUsingRegEX
     public class UserRegistration
     {
         //Regular expression
-        public static string reg = "(?=.*[A-Z])(?=.*[0-9]).{8}";
+        public static string reg = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]{1}).{8}";
         public static bool validate(string word)
         {
             //return true if the word matches the regex
