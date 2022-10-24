d0d6ed3203f2afe3211d18d2d994fb408377ab2d
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-24 19:45:09 +0530

    Validating User

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
-
 namespace UserRegistrationUsingRegEX
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To User registration Problem");
+            // Console.WriteLine("Welcome To User registration Problem");
 
+            Console.WriteLine("Enter the first name");
+            String FName = Console.ReadLine();
+
+            if (UserRegistration.Validate(FName))
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
         }
+
+
     }
 }
---
+++ UserRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingRegEX
+{
+    public class UserRegistration
+    {
+        public static string reg = "^[A-Z][A-za-z]{2,}";
+
+        public static bool Validate(String word)
+        {
+            return Regex.IsMatch(word, reg);
+        }
+
+    }
+}
